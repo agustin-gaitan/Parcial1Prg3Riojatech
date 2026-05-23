4908f8d68f7c56a8ea5a60c47ddba15ed22a1bc4
agustin-gaitan <agusgaitan141@gmail.com>
2026-05-23 09:02:34 -0300

    parcial1

---
+++ src/main/java/unlar/edu/ar/parcial1prg3riojatech/Parcial1prg3riojatechApplication.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.parcial1prg3riojatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial1prg3riojatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial1prg3riojatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unlar/edu/ar/parcial1prg3riojatech/Parcial1prg3riojatechApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.parcial1prg3riojatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial1prg3riojatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
